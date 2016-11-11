2b63d9c395b030d19be56c06f7d56e9bfc3000c9
Cooper Luetje <cooper@Juans-iMac.local>
2016-11-10 22:51:22 -0500

    Worked on the GameService

--- BaseballSim/GameService.swift
+++ BaseballSim/GameService.swift
@@ -60,8 +60,145 @@ class GameService
         return game
     }
     
-    func nextEvent()
+    func nextEvent(game:Game)
     {
+        let game_id = game.id
+        
+        let request = DispatchGroup.init()
+        
+        var url = apiRoutes.game.nextEvent
+        
+        //Add id to url
+        let itemId = ("/" + String(game_id)).characters.reversed()
+        
+        for i in itemId.indices
+        {
+            url.insert(itemId[i], at: apiRoutes.approvals.indexForId())
+        }
+        
+        //player1 and player2 ids
+        //params = []
+        
+        request.enter()
+        
+        requests.postRequest(url: url, params: (params as [String : AnyObject]?)!, headers: headers, finished: {
+            () in
+            request.leave()
+        })
+        
+        request.wait()
+        /*
+        // If games exist in the returned data from POST
+        if (requests.postDictionary["game"]! as AnyObject).count != 0
+        {
+            let val = requests.postDictionary.value(forKey: "game")! as AnyObject
+            let id = val.value(forKey: "id") as! Int
+            let league_id = val.value(forKey: "league_id")! as! Int
+            let field_id = val.value(forKey: "field_id")! as! Int
+            let team1_id = val.value(forKey: "team1_id")! as! Int
+            let team2_id = val.value(forKey: "team2_id")! as! Int
+            let date_created = val.value(forKey: "date_created") as! String
+            game = Game(id: id, league_id: league_id, field_id: field_id, team1_id: team1_id, team2_id: team2_id, date_created: date_created)
+        }
+        return game
+        */
+    }
+    
+    func getLatestEvent(game:Game) -> GameAction
+    {
+        let game_id = game.id
+        var gameAction:GameAction = GameAction(id: -1, game_id: -1, team_at_bat: -1, team1_score: -1, team2_score: -1, balls: -1, strikes: -1, outs: -1, inning: -1, type: "", message: "", date_created: "")
+        
+        let request = DispatchGroup.init()
+        
+        var url = apiRoutes.game.getLatestEvent
+        
+        //Add id to url
+        let itemId = ("/" + String(game_id)).characters.reversed()
+        
+        for i in itemId.indices
+        {
+            url.insert(itemId[i], at: apiRoutes.approvals.indexForId())
+        }
+        
+        //params = []
+        
+        request.enter()
+        
+        requests.getRequest(url: url, params: (params as [String : AnyObject]?)!, headers: headers, finished: {
+            () in
+            request.leave()
+        })
+        
+        request.wait()
+        
+         // If games exist in the returned data from GET
+         if (requests.getDictionary["game_id"]! as AnyObject).count != 0
+         {
+            let id = requests.getDictionary.value(forKey: "id")! as! Int
+            let game_id = requests.getDictionary.value(forKey: "game_id")! as! Int
+            let team_at_bat = requests.getDictionary.value(forKey: "team_at_bat")! as! Int
+            let team1_score = requests.getDictionary.value(forKey: "team1_score")! as! Int
+            let team2_score = requests.getDictionary.value(forKey: "team2_score")! as! Int
+            let balls = requests.getDictionary.value(forKey: "balls")! as! Int
+            let strikes = requests.getDictionary.value(forKey: "strikes")! as! Int
+            let outs = requests.getDictionary.value(forKey: "outs")! as! Int
+            let inning = requests.getDictionary.value(forKey: "inning")! as! Int
+            let type = requests.getDictionary.value(forKey: "type")! as! String
+            let message = requests.getDictionary.value(forKey: "message")! as! String
+            let date_created = requests.getDictionary.value(forKey: "date_created")! as! String
+            gameAction = GameAction(id: id, game_id: game_id, team_at_bat: team_at_bat, team1_score: team1_score, team2_score: team2_score, balls: balls, strikes: strikes, outs: outs, inning: inning, type: type, message: message, date_created: date_created)
+         }
+         return gameAction
+        
+    }
+    
+    func getEvents(game:Game) -> GameAction
+    {
+        let game_id = game.id
+        var gameAction:GameAction = GameAction(id: -1, game_id: -1, team_at_bat: -1, team1_score: -1, team2_score: -1, balls: -1, strikes: -1, outs: -1, inning: -1, type: "", message: "", date_created: "")
+        
+        let request = DispatchGroup.init()
+        
+        var url = apiRoutes.game.getAllEvents
+        
+        //Add id to url
+        let itemId = ("/" + String(game_id)).characters.reversed()
+        
+        for i in itemId.indices
+        {
+            url.insert(itemId[i], at: apiRoutes.approvals.indexForId())
+        }
+        
+        //params = []
+        
+        request.enter()
+        
+        requests.getRequest(url: url, params: (params as [String : AnyObject]?)!, headers: headers, finished: {
+            () in
+            request.leave()
+        })
+        
+        request.wait()
+        
+        // If games exist in the returned data from GET
+        if (requests.getDictionary["game_id"]! as AnyObject).count != 0
+        {
+            let id = requests.getDictionary.value(forKey: "id")! as! Int
+            let game_id = requests.getDictionary.value(forKey: "game_id")! as! Int
+            let team_at_bat = requests.getDictionary.value(forKey: "team_at_bat")! as! Int
+            let team1_score = requests.getDictionary.value(forKey: "team1_score")! as! Int
+            let team2_score = requests.getDictionary.value(forKey: "team2_score")! as! Int
+            let balls = requests.getDictionary.value(forKey: "balls")! as! Int
+            let strikes = requests.getDictionary.value(forKey: "strikes")! as! Int
+            let outs = requests.getDictionary.value(forKey: "outs")! as! Int
+            let inning = requests.getDictionary.value(forKey: "inning")! as! Int
+            let type = requests.getDictionary.value(forKey: "type")! as! String
+            let message = requests.getDictionary.value(forKey: "message")! as! String
+            let date_created = requests.getDictionary.value(forKey: "date_created")! as! String
+            gameAction = GameAction(id: id, game_id: game_id, team_at_bat: team_at_bat, team1_score: team1_score, team2_score: team2_score, balls: balls, strikes: strikes, outs: outs, inning: inning, type: type, message: message, date_created: date_created)
+        }
+        return gameAction
         
     }
 }
